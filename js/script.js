d627c08b7690b82d86a1c539385f9048ace03dae
edge <example@gmail.com>
2020-12-14 18:22:16 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+
+//let number = 5.3;
+//console.log(4/0);
+//console.log('string' * 9);
+
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+
+// const obj = {
+//     name: 'Rahmatilla',
+//     age: 23,
+//    siMaried: false
+// };
+// console.log(obj.name);
+
+//const result = confirm("qwerty");
+
+//const answer = prompt('are you 18?', '18');
+//console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('kak vashe imiya?','');
+// answers[1] = prompt('kak vashe faliliya?','');
+// answers[2] = prompt('skolko vam let?','');
+
+// console.log(typof(answers));
+
+// const category ='toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+// const user = 'rahmatilla';
+
+// alert(`hi, ${user}`)
+
+
+console.log('arr' + " - object");
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+const isChaked = true,
+    isClose = false;
+
+console.log(isChaked || isClose);
